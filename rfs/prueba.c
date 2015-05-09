23f86fa156ac94900cd0482acd042ed66c02f0bb
ggalvano <gonzalo_gal87@hotmail.com>
2015-05-09 05:36:33 -0300

    correcciones varias en prueba.c

--- rfs/prueba.c
+++ rfs/prueba.c
@@ -1,1 +1,22 @@
 //prueba
+
+#include <stdio.h>
+
+void mostrarMensaje(char*);
+
+int main(){
+	char buffer[80];
+
+	mostrarMensaje("ingrese su texto:\n");
+	scanf("%s", buffer);
+	printf("El texto ingresado:",buffer);
+
+
+
+
+return 0;
+}
+
+void mostrarMensaje(char* mensaje){
+	printf("%s", mensaje);
+}
