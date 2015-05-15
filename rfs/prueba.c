9a2092954168721f3eae9cd917dda1566c7342d1
ggalvano <gonzalo_gal87@hotmail.com>
2015-05-15 15:15:37 -0300

    varios config

--- rfs/prueba.c
+++ rfs/prueba.c
@@ -1,17 +1,24 @@
 //prueba
 
 #include <stdio.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <cspecs/cspec.h>
+#include <stdlib.h>
+#include <string.h>
 
 void mostrarMensaje(char*);
 
 int main(){
-	char buffer[512];
+	//char buffer[512];
+	char* ip;
 
+	t_config* config;
+	config = config_create("/home/utnso/Escritorio/git/prueba/resourses/config.cfg");
+	ip = config_get_string_value(config,"IP");
 	mostrarMensaje("Ingrese el texto:\n");
-	scanf("%s", buffer);
-	printf("El texto ingresado fue: '%s' \n",buffer);
-
-
+	//scanf("%s", buffer);
+	printf("El texto ingresado fue: '%s' \n",ip);
 
 
 return 0;
